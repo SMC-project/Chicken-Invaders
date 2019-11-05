8c4396458a94ad25e63e5feb183865e40f09298a
Ionut Opria <opriaionut14@gmail.com>
2019-11-05 16:42:13 +0200

    Adding player input
    Added the player input for the other arrow keys. Also added a method that checks for collisions so that the player can't leave the playable area.
    Up until now all the commits implemented functionality that we will all have to use during development. We made these commits together so that we could have a starting point.

--- Chicken Invaders/Chicken Invaders/Source.cpp
+++ Chicken Invaders/Chicken Invaders/Source.cpp
@@ -4,6 +4,7 @@
 
 void GameLoop();
 void CheckForInput(Map& myMap, std::pair<int, int> newPlayerPos, std::pair<int, int>& playerPos);
+bool CollisionCheck(const Map& myMap, std::pair<int, int> newPos);
 
 int main()
 {
@@ -21,20 +22,63 @@ void GameLoop()
 
 	while (stopGame == false)
 	{
-		
+		CheckForInput(myMap, newPlayerPos, playerPos);
 		//Sleep(500);
 	}
 }
 
 void CheckForInput(Map& myMap, std::pair<int, int> newPlayerPos, std::pair<int, int>& playerPos)
 {
-	if (GetAsyncKeyState(VK_DOWN))
+	if (GetAsyncKeyState(VK_UP))
 	{
 		newPlayerPos = playerPos;
-		newPlayerPos.first++;
+		newPlayerPos.first--;
+		if (CollisionCheck(myMap, newPlayerPos) == false)
+			return;
 
 		myMap.MoveChar(playerPos, newPlayerPos, '@');
 		playerPos = newPlayerPos;
 		myMap.Redraw();
 	}
+	if (GetAsyncKeyState(VK_DOWN))
+	{
+		newPlayerPos = playerPos;
+		newPlayerPos.first++;
+		if (CollisionCheck(myMap, newPlayerPos) == false)
+			return;
+
+		myMap.MoveChar(playerPos, newPlayerPos, '@');
+		playerPos = newPlayerPos;
+		myMap.Redraw();
+	}
+	if (GetAsyncKeyState(VK_LEFT))
+	{
+		newPlayerPos = playerPos;
+		newPlayerPos.second--;
+		if (CollisionCheck(myMap, newPlayerPos) == false)
+			return;
+
+		myMap.MoveChar(playerPos, newPlayerPos, '@');
+		playerPos = newPlayerPos;
+		myMap.Redraw();
+	}
+	if (GetAsyncKeyState(VK_RIGHT))
+	{
+		newPlayerPos = playerPos;
+		newPlayerPos.second++;
+		if (CollisionCheck(myMap, newPlayerPos) == false)
+			return;
+
+		myMap.MoveChar(playerPos, newPlayerPos, '@');
+		playerPos = newPlayerPos;
+		myMap.Redraw();
+	}
+}
+
+//Returns true if it can move to that position
+bool CollisionCheck(const Map& myMap, std::pair<int, int> newPos)
+{
+	if (myMap.map[newPos.first][newPos.second] == ' ')
+		return true;
+	return false;
 }
