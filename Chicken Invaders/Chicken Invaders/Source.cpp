73078bbc52bf077780494edea5bc1f9bc2bf9dbb
Ionut Opria <opriaionut14@gmail.com>
2019-11-05 16:35:15 +0200

    Project Creation
    Created the project, the game loop where all the functionality will be called from and added intermediary coordinates for the player to test basic functionality. In later versions it will be part of it's own class.

---
+++ Chicken Invaders/Chicken Invaders/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+	GameLoop();
+	system("pause");
+}
+
+void GameLoop()
+{
+	std::pair<int, int> newPlayerPos;
+	std::pair<int, int> playerPos(1, 2);
+	bool stopGame = false;
+
+	while (stopGame == false)
+	{
+		
+		//Sleep(500);
+	}
+}
