6339ed0c27116e7794b678c3c411205fc06354af
Tudor21 <21popescutudor21@gmail.com>
2019-11-14 22:34:07 +0200

    Chicken Movement at wave 1
    I have implemented a function called movementFirstWave which is moving every chicken from the vector from left to right and viceversa between 2 given coordonates.

--- Chicken Invaders/Chicken Invaders/Wave.h
+++ Chicken Invaders/Chicken Invaders/Wave.h
@@ -4,8 +4,10 @@ class Wave
 {
 public:
 	Chicken chicken[5][8];
+	bool left_or_right_movement = true;
 	void fisrtWavePosition(Chicken chicken[5][8]);
 	void drawWave(RenderWindow& map,Chicken chicken[5][8]);
 	void setSprite(Texture& enemy,Chicken chicken[5][8]);
+	void movementFirstWave(Chicken chicken[5][8]);
 };
 
