163e4ee1ab1dc159b2f717cbcb687286c595df08
TheodorMoldovan <46047152+TheodorMoldovan@users.noreply.github.com>
2019-11-22 22:28:06 +0200

    [Edit][Add]
    Am editat anumite functii din clasa bullet si am adaugat functii noi. Urmeaza sa implementez un move constructur necesar pentru adaugarea mai multor obiecte de tip glont;

--- Chicken Invaders/Chicken Invaders/Bullet.cpp
+++ Chicken Invaders/Chicken Invaders/Bullet.cpp
@@ -1,112 +1,129 @@
 #include "Bullet.h"
 #include<Windows.h>
+
 void Bullet::LevelPower(Level currentLevel)
 {
 	switch (currentLevel)
 	{
 	case Level::Level_0:
-		m_bullets[0].second = false;
-		m_bullets[1].second = false;
-		m_bullets[2].second = false;
-		m_bullets[3].second = true;
-		m_bullets[4].second = false;
-		m_bullets[5].second = false;
-		m_bullets[6].second = false;
+		std::get<2>(m_bullets[0]) = false;
+		std::get<2>(m_bullets[1]) = false;
+		std::get<2>(m_bullets[2]) = false;
+		std::get<2>(m_bullets[3]) = true;
+		std::get<2>(m_bullets[4]) = false;
+		std::get<2>(m_bullets[5]) = false;
+		std::get<2>(m_bullets[6]) = false;
 	case Level::Level_1:
-		m_bullets[0].second = false;
-		m_bullets[1].second = false;
-		m_bullets[2].second = true;
-		m_bullets[3].second = false;
-		m_bullets[4].second = true;
-		m_bullets[5].second = false;
-		m_bullets[6].second = false;
+		std::get<2>(m_bullets[0]) = false;
+		std::get<2>(m_bullets[1]) = false;
+		std::get<2>(m_bullets[2]) = true;
+		std::get<2>(m_bullets[3]) = false;
+		std::get<2>(m_bullets[4]) = true;
+		std::get<2>(m_bullets[5]) = false;
+		std::get<2>(m_bullets[6]) = false;
 		break;
 	case Level::Level_2:
-		m_bullets[0].second = false;
-		m_bullets[1].second = false;
-		m_bullets[2].second = true;
-		m_bullets[3].second = true;
-		m_bullets[4].second = true;
-		m_bullets[5].second = false;
-		m_bullets[6].second = false;
+		std::get<2>(m_bullets[0]) = false;
+		std::get<2>(m_bullets[1]) = false;
+		std::get<2>(m_bullets[2]) = true;
+		std::get<2>(m_bullets[3]) = true;
+		std::get<2>(m_bullets[4]) = true;
+		std::get<2>(m_bullets[5]) = false;
+		std::get<2>(m_bullets[6]) = false;
 		break;
 	case Level::Level_3:
-		m_bullets[0].second = false;
-		m_bullets[1].second = true;
-		m_bullets[2].second = false;
-		m_bullets[3].second = true;
-		m_bullets[4].second = false;
-		m_bullets[5].second = true;
-		m_bullets[6].second = false;
+		std::get<2>(m_bullets[0]) = false;
+		std::get<2>(m_bullets[1]) = true;
+		std::get<2>(m_bullets[2]) = false;
+		std::get<2>(m_bullets[3]) = true;
+		std::get<2>(m_bullets[4]) = false;
+		std::get<2>(m_bullets[5]) = true;
+		std::get<2>(m_bullets[6]) = false;
 		break;
 	case Level::Level_4:
-		m_bullets[0].second = true;
-		m_bullets[1].second = true;
-		m_bullets[2].second = false;
-		m_bullets[3].second = false;
-		m_bullets[4].second = false;
-		m_bullets[5].second = true;
-		m_bullets[6].second = true;
+		std::get<2>(m_bullets[0]) = true;
+		std::get<2>(m_bullets[1]) = true;
+		std::get<2>(m_bullets[2]) = false;
+		std::get<2>(m_bullets[3]) = false;
+		std::get<2>(m_bullets[4]) = false;
+		std::get<2>(m_bullets[5]) = true;
+		std::get<2>(m_bullets[6]) = true;
 		break;
 	case Level::Level_5:
-		m_bullets[0].second = true;
-		m_bullets[1].second = true;
-		m_bullets[2].second = false;
-		m_bullets[3].second = true;
-		m_bullets[4].second = false;
-		m_bullets[5].second = true;
-		m_bullets[6].second = true;
+		std::get<2>(m_bullets[0]) = true;
+		std::get<2>(m_bullets[1]) = true;
+		std::get<2>(m_bullets[2]) = false;
+		std::get<2>(m_bullets[3]) = true;
+		std::get<2>(m_bullets[4]) = false;
+		std::get<2>(m_bullets[5]) = true;
+		std::get<2>(m_bullets[6]) = true;
 		break;
 	case Level::Level_6:
-		m_bullets[0].second = true;
-		m_bullets[1].second = true;
-		m_bullets[2].second = true;
-		m_bullets[3].second = false;
-		m_bullets[4].second = true;
-		m_bullets[5].second = true;
-		m_bullets[6].second = true;
+		std::get<2>(m_bullets[0]) = true;
+		std::get<2>(m_bullets[1]) = true;
+		std::get<2>(m_bullets[2]) = true;
+		std::get<2>(m_bullets[3]) = false;
+		std::get<2>(m_bullets[4]) = true;
+		std::get<2>(m_bullets[5]) = true;
+		std::get<2>(m_bullets[6]) = true;
 		break;
 	case Level::Level_7:
-		m_bullets[0].second = true;
-		m_bullets[1].second = true;
-		m_bullets[2].second = true;
-		m_bullets[3].second = true;
-		m_bullets[4].second = true;
-		m_bullets[5].second = true;
-		m_bullets[6].second = true;
+		std::get<2>(m_bullets[0]) = true;
+		std::get<2>(m_bullets[1]) = true;
+		std::get<2>(m_bullets[2]) = true;
+		std::get<2>(m_bullets[3]) = true;
+		std::get<2>(m_bullets[4]) = true;
+		std::get<2>(m_bullets[5]) = true;
+		std::get<2>(m_bullets[6]) = true;
 		break;
 	}
-	
+
 }
-Bullet::Bullet(std::pair<int, int> shipCenterPosition)
+Bullet::Bullet(int shipCenterPosition_x, int shipCenterPosition_y)
 {
-	m_bulletLevel = Level::Level_0;
-	for (int index = 0; index < 7; index++)
+	m_bulletTexture.loadFromFile("Sprites/Weapons/bullet.png");
+	sf::Sprite auxSprite;
+	auxSprite.setTexture(m_bulletTexture);
+	auxSprite.setScale(0.12, 0.12);
+	std::pair<int, int>bulletposition;
+	for (int i = 0; i < 7; i++)
 	{
-		m_bullets[index].first.first = shipCenterPosition.first - 4 + index;
-		m_bullets[index].first.second = shipCenterPosition.second - 1;
-		m_bullets[index].second = false;
+		m_bulletSprites.push_back(auxSprite);
 	}
-	m_bullets[4].second = true;
-	
+	for (int i = 0; i < 7; i++)
+	{
+		m_bullets.push_back(std::make_tuple(1000, 1000, false));
+	}
+	m_bulletLevel = Level::Level_0;
+	std::get<2>(m_bullets[4]) = true;
+	m_bulletSprites[4].setPosition(shipCenterPosition_x, shipCenterPosition_y - 10);
+	//BulletsPosition_Update();
 }
 
 void Bullet::Present_Collected()
 {
 	m_bulletLevel;
-	int currentLevel =static_cast<int>(m_bulletLevel);
+	int currentLevel = static_cast<int>(m_bulletLevel);
 	if (currentLevel < 6)
 	{
 		currentLevel++;
 		m_bulletLevel = static_cast<Level>(currentLevel);
 		LevelPower(m_bulletLevel);
-    }
+	}
 
 }
 
-void Bullet::BulletsPosition_Update()
+void Bullet::BulletsPosition_Update(int& currentX, int& currentY)
 {
 
+	for (int i = 0; i < m_bullets.size(); i++)
+	{
+		currentY -= 2;
+		if (std::get<2>(m_bullets[i]) == true)
+		{
+			m_bulletSprites[i].setPosition(currentX, currentY);
+		}
+	}
 
 }
 
@@ -114,4 +131,15 @@ void Bullet::ResetBulletLevel()
 {
 	m_bulletLevel = Level::Level_0;
 	LevelPower(m_bulletLevel);
+}
+
+void Bullet::DrawBullet(sf::RenderWindow& gameWindow)
+{
+	for (int i = 0; i < m_bullets.size(); i++)
+	{
+		if (std::get<2>(m_bullets[i]) == true)
+		{
+			gameWindow.draw(m_bulletSprites[i]);
+		}
+	}
 }
--- Chicken Invaders/Chicken Invaders/Bullet.h
+++ Chicken Invaders/Chicken Invaders/Bullet.h
@@ -1,15 +1,17 @@
 #pragma once
 #include<iostream>
 #include<vector>
-
-enum class Level {Level_0, Level_1, Level_2, Level_3, Level_4, Level_5, Level_6, Level_7 };
+#include<SFML/Graphics.hpp>
+enum class Level { Level_0, Level_1, Level_2, Level_3, Level_4, Level_5, Level_6, Level_7 };
 
 class Bullet
 {
 private:
 	Level m_bulletLevel;
-	std::vector <std::pair<std::pair<int, int>, bool>> m_bullets;
+	std::vector <std::tuple<int, int, bool>> m_bullets;
 	float m_bulletSpeed = 100;
+	std::vector<sf::Sprite> m_bulletSprites;
+	sf::Texture m_bulletTexture;
 
 protected:
 	//Functie ce se apeleaza in Present_Collected
@@ -17,12 +19,14 @@ protected:
 
 public:
 	//Constructorul primeste varful navei;
-	Bullet(std::pair<int, int> shipCenterPosition);
+	Bullet(int shipCenterPosition_x, int shipCenterPosition_y);
 	//Apelezi in momentul in care nava se intersecteaza cu un Present_Collected pentru a afisa noi gloante;
 	void Present_Collected();
 	//Pozitia in care se afla gloantele la fiecare 0,1 secunde;
-	void BulletsPosition_Update();
+	void BulletsPosition_Update(int& currentX, int& currentY);
 	//Nava a fost distrusa
 	void ResetBulletLevel();
+	void Shot();
+	void DrawBullet(sf::RenderWindow& gameWindow);
 };
 
