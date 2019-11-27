4584c4a7e98d91a9ad10c5e52fcb77af63cd9119
Tudor21 <21popescutudor21@gmail.com>
2019-11-27 10:35:02 +0200

    Wave3 and sound
    I have added music background and wave 3 in which asteroids randomly spawns and fall .

--- Chicken Invaders/Chicken Invaders/Wave.h
+++ Chicken Invaders/Chicken Invaders/Wave.h
@@ -2,20 +2,28 @@
 #include"Chicken.h"
 #include "Asteroid.h"
 #include"Explosion.h"
-
 #include "Present.h"
+#include<time.h>
 
 class Wave
 {
-public:
-	Chicken chicken[5][8];
-
+private:
+	//used in the first wave changing the state of direction
 	bool left_or_right_movement = true;
+	//used to get random initial position
+	int random_number;
+public:
+	int wave_number = 1;
+
 	//this is the first wave where we have 5 x 6 chickens
 	void fisrtWavePosition(Chicken chicken[5][8], const int WINDOW_WIDTH, const int WINDOW_HEIGHT);
 	void drawWave(RenderWindow& map,Chicken chicken[5][8]);
 	void setSprite(Texture& enemy,Chicken chicken[5][8]);
-	void movementFirstWave(Chicken chicken[5][8]);
+	void movementFirstWave(Chicken chicken[5][8], Wave& wave3);
 
+	void drawWave_asteroid(RenderWindow& map, Asteroid asteroidVect[5][5]);
+	void setSprite_asteroid1(Texture& asteroid, Asteroid asteroidVect[5][5]);
+	void thirdWave_Position(Asteroid asteroidVect[5][5]);
+	void thirdWave_Movement(Asteroid asteroidVect[5][5]);
 };
 
